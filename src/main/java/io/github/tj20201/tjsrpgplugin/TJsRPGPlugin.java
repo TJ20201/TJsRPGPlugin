014422a6f85b16cae573da242f5931cdf2560089
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2023-02-22 10:31:27 +0000

    Initial commit

---
+++ src/main/java/io/github/tj20201/tjsrpgplugin/TJsRPGPlugin.java
@@ -0,0 +1,17 @@
+package io.github.tj20201.tjsrpgplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TJsRPGPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
